6f1e5c17881197016c7639974a2fc63826e36687
Kaina Cockett <kainacockett@gmail.com>
2023-09-05 11:24:27 -0600

    Lesson complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "welcome#index"
+  resources :users, only: [:new, :create]
+  # this is not RESTful, and that's OKAY
+	get "/login", to: "users#login_form"
+	post "/login", to: "users#login"
 end
